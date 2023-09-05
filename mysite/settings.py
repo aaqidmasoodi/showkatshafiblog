1fc9b3869cdd1bb53a938d6f0075707b2ae032a0
aaqidmasoodi <aaqidmasoodi@icloud.com>
2023-09-05 16:27:26 +0530

    checking vercel configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -66,9 +66,13 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clkjiycp',
+        'USER': 'clkjiycp',
+        'PASSWORD': 'iAH125s7aAH8oNBjrpkyOQ3SYY_UYHDt',
+        'HOST': 'satao.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 
